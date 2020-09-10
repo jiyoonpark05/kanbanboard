a63465c07b300c736476b79196c09381ae32f1ce
jiyoonPark <jiyoonpark05@gmail.com>
2020-09-10 22:50:23 +0900

    create Redux Store

--- src/App.js
+++ src/App.js
@@ -3,19 +3,23 @@ import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route} from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import AddProject from "./components/Projects/AddProject";
+import { Provider } from "react-redux"; //how we define the store. we have to wrap entire application with provider tag
+import store from "./store";
 
 class App extends Component {
   render() {
     return (
+      <Provider store={store}>
         <Router>
           <div className="App">
             <Header />
-            <Route exact path="/dashboard" component={Dashboard}/>
-            <Route exact path="/addProject" component={AddProject}/>
+            <Route exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/addProject" component={AddProject} />
           </div>
         </Router>
+      </Provider>
     );
   }
 }
